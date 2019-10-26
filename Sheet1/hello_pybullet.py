1385e6943ad645303e0c09544570e260cf796fd0
Leif Van Holland <holland@uni-bonn.de>
2019-10-26 13:34:18 +0200

    fix example indentation

--- Sheet1/hello_pybullet.py
+++ Sheet1/hello_pybullet.py
@@ -15,11 +15,11 @@ cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
 useRealTimeSimulation = 1
 
 if useRealTimeSimulation:
-  p.setRealTimeSimulation(1)
+    p.setRealTimeSimulation(1)
 
 while 1:
-  if useRealTimeSimulation:
-    p.setGravity(0, 0, -10)
-    sleep(0.01)  # Time in seconds.
-  else:
-    p.stepSimulation()
+    if useRealTimeSimulation:
+        p.setGravity(0, 0, -10)
+        sleep(0.01)  # Time in seconds.
+    else:
+        p.stepSimulation()
